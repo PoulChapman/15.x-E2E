2797282bd803d522822166fde42afc4a73fad4a1
Phillip Carter <phillip.carter@outlook.com>
2017-03-28 15:10:00 -0700

    Initial commit

---
+++ E2E.API/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using E2E.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace E2E.API.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values/some-word-to-turn-to-pig-latin
+        public string Get(string word)
+        {
+            return PigLatin.ToPigLatin(word);
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ E2E.Forms/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json
+
+Public Class Form1
+
+    Private ReadOnly client As HttpClient = New HttpClient()
+
+    Private Sub GenerateButton_Click(sender As Object, e As EventArgs) Handles GenerateButton.Click
+        HandleIt()
+    End Sub
+
+    Private Sub InputField_KeyDown(sender As Object, e As KeyEventArgs) Handles InputField.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            HandleIt()
+        End If
+    End Sub
+
+    Private Sub HandleIt()
+        Dim input As String = InputField.Text
+        Dim pigLatin As String = Core.PigLatin.ToPigLatin(input)
+        ResultLabel.Text = $"{input} -> {pigLatin}"
+    End Sub
+
+    Private Async Sub WebServiceButton_Click(sender As Object, e As EventArgs) Handles WebServiceButton.Click
+        Dim url As String = $"http://localhost:53303/api/values?word={InputField.Text}"
+        Dim json = Await client.GetStringAsync(url)
+        Dim result = JsonConvert.DeserializeObject(Of String)(json)
+        ResultLabel.Text = $"{InputField.Text} -> {result}"
+    End Sub
+End Class
